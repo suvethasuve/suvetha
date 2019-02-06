714656538cae058b635798d63fdfc5abfd0fd7fb
suvethasuve <47360600+suvethasuve@users.noreply.github.com>
2019-02-06 12:32:02 +0530

    Create py4.py

---
+++ py4.py
@@ -0,0 +1,6 @@
+character = input()
+if(character=='A' or character=='a' or character=='E' or character =='e' or character=='I'
+ or character=='i' or character=='O' or character=='o' or character=='U' or character=='u'):
+    print(character, "is a Vowel")
+else:
+    print(character, "is a Consonant")
