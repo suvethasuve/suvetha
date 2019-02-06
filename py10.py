318f80d51535965c15506f5612359521794db431
suvethasuve <47360600+suvethasuve@users.noreply.github.com>
2019-02-06 15:43:54 +0530

    Update py10.py

--- py10.py
+++ py10.py
@@ -1,6 +1,9 @@
-n=(input())
-count=0
-while(n>0):
-    count=count+1
-    n=n//10
-print(count)
+try:
+	n=int(input())
+	c=0
+	while(n!=0):
+		n=int(n/10)
+		c=c+1
+	print(c)
+except:
+	print('invalid')
